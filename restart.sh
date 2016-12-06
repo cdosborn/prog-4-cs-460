171ca55cf43e6d959cd066dc33a5b0ad0282f38a
Connor Osborn <cdosborn@email.arizona.edu>
2016-12-06 10:00:28 -0700

    DELME

--- restart.sh
+++ restart.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-/usr/local/jdk/bin/javac -cp ../lib/*: ROOT/WEB-INF/classes/controller/*java
+/usr/local/jdk/bin/javac -cp ../lib/*:ROOT/WEB-INF/lib/* ROOT/WEB-INF/classes/controller/*java
 ../bin/shutdown.sh;
 ../bin/startup.sh;
