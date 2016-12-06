083d813849266acbee96eec39513353cd692bdef
Connor Osborn <cdosborn@email.arizona.edu>
2016-12-06 08:30:26 -0700

    Include jstl jar

--- restart.sh
+++ restart.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-/usr/local/jdk/bin/javac -cp ~/tomcat/lib/*: ROOT/WEB-INF/classes/controller/*java
+/usr/local/jdk/bin/javac -cp ~/tomcat/lib/*:ROOT/WEB-INF/lib/* ROOT/WEB-INF/classes/controller/*java
 ~/tomcat/bin/shutdown.sh;
 ~/tomcat/bin/startup.sh;
